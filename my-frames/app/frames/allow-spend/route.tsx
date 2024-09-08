0b391477c24f56793803ff588c0efd394638bef2
Sweta Shaw <swt.shw94@gmail.com>
2024-09-08 19:48:53 +0530

    fix: parseUnits

--- my-frames/app/frames/allow-spend/route.tsx
+++ my-frames/app/frames/allow-spend/route.tsx
@@ -1,8 +1,7 @@
 //@ts-nocheck
 import { frames } from "../frames";
-import { createPublicClient, encodeFunctionData, getContract, http } from "viem";
-import { baseSepolia } from "viem/chains";
-import XmtpP2PAbi from "../../contract-abi.json";
+import { transaction } from "frames.js/core";
+import { encodeFunctionData } from "viem";
 import USDCAbi from "../../MockUSDC.json";
 import { ethers } from "ethers";
 
@@ -11,16 +10,8 @@ const contractAddress = '0x648eAcAa1C7FEbb06f9b682603fFC6d20b97450b'; // P2P con
 const usdcContractAddress = '0x254Ff46538bf3C581435850F73D2f26011417a1C'; // USDC contract address
 
 export const POST = frames(async (ctx) => {
-    const publicClient = createPublicClient({
-        chain: baseSepolia,
-        transport: http(),
-    });
-
-
-    const walletAddress = ctx?.message?.walletAddress;
     const amount = "1";
-    const parsedAmount = ethers.parseUnits(amount, 6);
-
+    const parsedAmount = ethers.utils.parseUnits(amount, 6);
 
     const data = encodeFunctionData({
         abi: USDCAbi.abi,
@@ -28,24 +19,12 @@ export const POST = frames(async (ctx) => {
         args: [contractAddress, parsedAmount],
     });
 
-    try {
-        // Initiate the transaction to approve spending on behalf of the user
-        const txResponse = await publicClient.sendTransaction({
+    return transaction({
+        chainId: "eip155:84532", // base sepolia
+        method: "eth_sendTransaction",
+        params: {
             to: usdcContractAddress,
-            from: walletAddress,
-            data,
-        });
-
-        console.log(`Transaction sent: ${txResponse.hash}`);
-
-
-        return {
-            image: <span>Approved {amount} USDC for P2P contract. View on <a href={`https://sepolia.etherscan.io/tx/${txResponse.hash}`} target="_blank" rel="noopener noreferrer">Etherscan</a></span>,
-        };
-    } catch (error) {
-        console.error("Error approving USDC spend:", error);
-        return {
-            image: <span>Error approving USDC spend: {error.message}</span>,
-        };
-    }
+            data
+        },
+    });
 });
